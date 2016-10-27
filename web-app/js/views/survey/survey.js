f85b60a0d189f8b25ba432fe350ffc34f28993b5
asism <Asis.Mohanty@ellucian.com>
2016-10-27 17:23:19 +0530

    commit after removing the about dialogue pages

--- web-app/js/views/survey/survey.js
+++ web-app/js/views/survey/survey.js
@@ -2,34 +2,6 @@
  Copyright 2016 Ellucian Company L.P. and its affiliates.
  ****************************************************************************** */
 $(document).ready(function () {
-    try{
-        if(angular.module("aboutModal")){
-            var dialogDiv = document.getElementById('dialogAppDiv');
-            dialogDiv.setAttribute("ng-app","dialogApp");
-            dialogDiv.setAttribute("ng-controller","ModalCtrl");
-            dialogDiv.innerHTML = "<xe-about-modal show='modalShown' api='aboutApi'></xe-about-modal>";
-            ToolsMenu.addItem(
-                "about",
-                $.i18n.prop("aurora.areas_label_about_title"),
-                "",
-                aboutDialogPopUp
-            );
-        }
-    } catch(e){
-        console.log('Not adding About menu item because aboutModal Module is not found in resource.');
-    }
-    function aboutDialogPopUp () {
-        var scope = angular.element(document.getElementById('dialogAppDiv')).scope();
-        if(!scope){
-            angular.element(document.getElementById('dialogAppDiv')).ready(function() {
-                angular.bootstrap(document.getElementById('dialogAppDiv'), ['dialogApp']);
-            });
-            scope = angular.element(document.getElementById('dialogAppDiv')).scope();
-        }
-        scope.$apply(function(){
-            scope.toggleModal();
-        })
-    }
     $('#confirmSurvey').hide();
     $('#chkEthn_1').focus();
     var notificationMessages = new Array();

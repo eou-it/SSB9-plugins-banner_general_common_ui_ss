fcb0c1290ef240d5f9d5bdb46d8579b1bc79a427
Harshal Bramhankar <harshal.bramhankar@ellucian.com>
2018-06-18 12:53:28 +0530

    HRU-7818 Changes for Filter to interceptor

--- src/main/scripts/_Uninstall.groovy
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
- Copyright 2014 Ellucian Company L.P. and its affiliates.
- *******************************************************************************/
-
-//
-// This script is executed by Grails when the plugin is uninstalled from project.
-// Use this script if you intend to do any additional clean-up on uninstall, but
-// beware of messing up SVN directories!
-//

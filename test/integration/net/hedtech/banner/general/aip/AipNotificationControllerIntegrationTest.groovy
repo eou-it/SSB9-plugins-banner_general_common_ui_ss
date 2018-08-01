73bc1cc820233cbe7fe4109d2993d29e0bb248ac
sivaramn <sivaram.nyapathi@ellucian.com>
2018-08-01 14:42:43 +0530

    NO JIRA : Incorporating review comments

--- test/integration/net/hedtech/banner/general/aip/AipNotificationControllerIntegrationTest.groovy
+++ test/integration/net/hedtech/banner/general/aip/AipNotificationControllerIntegrationTest.groovy
@@ -34,26 +34,28 @@ class AipNotificationControllerIntegrationTest extends BaseIntegrationTestCase {
 
     }
 
+
     @After
     public void tearDown() {
         logout()
         super.tearDown()
     }
+
+
     @Test
-    public void checkNotificationWhenNotLoggedIn(){
+    public void checkNotificationWhenNotLoggedIn() {
         logout()
         controller.checkNotification()
-        assertEquals  403, controller.response.status.value
-
-
+        assertEquals 403, controller.response.status.value
     }
 
+
     @Test
-    public void testCheckNotificationWhenAIPEnabled(){
-        def oldValue = getGoriicrValue( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS);
+    public void testCheckNotificationWhenAIPEnabled() {
+        def oldValue = getGoriicrValue(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS);
         assertNotNull oldValue
 
-        assertTrue updateGoriccrRule( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS,  YES)
+        updateGoriccrRule(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS, YES)
 
         // AIP enabled with action items and first time log in
         Authentication authentication = selfServiceBannerAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken('CSRSTU001', '111111'))
@@ -63,26 +65,25 @@ class AipNotificationControllerIntegrationTest extends BaseIntegrationTestCase {
         assertNotNull controller.response;
 
         def response = controller.response.contentAsString
-        assertEquals  200, controller.response.status.value
-        def data = JSON.parse( response )
+        assertEquals 200, controller.response.status.value
+        def data = JSON.parse(response)
 
         assertEquals 3, data.size()
         assertTrue data.hasActiveActionItems
         assertFalse data.isNotificationDisplayed
         assertNotNull data.url
 
-        assertEquals ENABLED,controller.session.request.session.attributes['aipEnabledStatus'].toString()
+        assertEquals ENABLED, controller.session.request.session.attributes['aipEnabledStatus'].toString()
         assertTrue controller.session.getAttribute('isNotificationShown').toBoolean()
-
-        assertTrue updateGoriccrRule( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS,  oldValue)
     }
 
+
     @Test
-    public void testCheckNotificationWhenAIPDisabled(){
-        def oldValue = getGoriicrValue( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS);
+    public void testCheckNotificationWhenAIPDisabled() {
+        def oldValue = getGoriicrValue(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS);
         assertNotNull oldValue
 
-        assertTrue updateGoriccrRule( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS,  NO)
+        updateGoriccrRule(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS, NO)
 
         // AIP enabled with action items and first time log in
         Authentication authentication = selfServiceBannerAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken('CSRSTU001', '111111'))
@@ -92,26 +93,25 @@ class AipNotificationControllerIntegrationTest extends BaseIntegrationTestCase {
         assertNotNull controller.response;
 
         def response = controller.response.contentAsString
-        assertEquals  200, controller.response.status.value
-        def data = JSON.parse( response )
+        assertEquals 200, controller.response.status.value
+        def data = JSON.parse(response)
 
         assertEquals 3, data.size()
         assertFalse data.hasActiveActionItems
         assertFalse data.isNotificationDisplayed
-        assertEquals JSONObject.NULL,data.url
+        assertEquals JSONObject.NULL, data.url
 
-        assertEquals DISABLED,controller.session.getAttribute('aipEnabledStatus').toString()
+        assertEquals DISABLED, controller.session.getAttribute('aipEnabledStatus').toString()
         assertTrue controller.session.getAttribute('isNotificationShown').toBoolean()
-
-        assertTrue updateGoriccrRule( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS,  oldValue)
     }
 
+
     @Test
-    public void testCheckNotificationAfterDismissing(){
-        def oldValue = getGoriicrValue( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS);
+    public void testCheckNotificationAfterDismissing() {
+        def oldValue = getGoriicrValue(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS);
         assertNotNull oldValue
 
-        assertTrue updateGoriccrRule( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS,  YES)
+        updateGoriccrRule(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS, YES)
 
         // AIP enabled with action items and first time log in
         Authentication authentication = selfServiceBannerAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken('CSRSTU001', '111111'))
@@ -123,24 +123,23 @@ class AipNotificationControllerIntegrationTest extends BaseIntegrationTestCase {
         controller.checkNotification()
 
         def secondResponse = controller.response.contentAsString
-        assertEquals  200, controller.response.status.value
-        def data2 = JSON.parse( secondResponse )
+        assertEquals 200, controller.response.status.value
+        def data2 = JSON.parse(secondResponse)
 
         assertEquals 3, data2.size()
         assertFalse data2.hasActiveActionItems
         assertTrue data2.isNotificationDisplayed
-        assertEquals JSONObject.NULL,data2.url
-
-        assertTrue updateGoriccrRule( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS,  oldValue)
+        assertEquals JSONObject.NULL, data2.url
     }
 
-    @Test
-    public void testCheckNotificationWhenAIPEnabledNoActionItems(){
 
-        def oldValue = getGoriicrValue( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS);
+    @Test
+    public void testCheckNotificationWhenAIPEnabledNoActionItems() {
+
+        def oldValue = getGoriicrValue(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS);
         assertNotNull oldValue
 
-        assertTrue updateGoriccrRule( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS,  YES)
+        updateGoriccrRule(SQPR_CODE_GENERAL_SSB, ICSN_CODE_ENABLE_ACTION_ITEMS, YES)
 
         // AIP enabled with action items and first time log in
         Authentication authentication = selfServiceBannerAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken('AIPADM001', '111111'))
@@ -150,68 +149,32 @@ class AipNotificationControllerIntegrationTest extends BaseIntegrationTestCase {
         assertNotNull controller.response;
 
         def response = controller.response.contentAsString
-        assertEquals  200, controller.response.status.value
-        def data = JSON.parse( response )
+        assertEquals 200, controller.response.status.value
+        def data = JSON.parse(response)
 
         assertEquals 3, data.size()
         assertFalse data.hasActiveActionItems
         assertFalse data.isNotificationDisplayed
         assertNotNull data.url
 
-        assertEquals ENABLED,controller.session.request.session.attributes['aipEnabledStatus'].toString()
+        assertEquals ENABLED, controller.session.request.session.attributes['aipEnabledStatus'].toString()
         assertTrue controller.session.getAttribute('isNotificationShown').toBoolean()
 
-        assertTrue updateGoriccrRule( SQPR_CODE_GENERAL_SSB,  ICSN_CODE_ENABLE_ACTION_ITEMS,  oldValue)
     }
-
 
 
     private def getGoriicrValue(def sqpr_code, def icsn_code) {
-        Sql sqlObj
-        def gorriccrValue
-        try {
-            sqlObj = new Sql(sessionFactory.getCurrentSession().connection())
-            def sqlQuery = """
-                            select g.goriccr_value
-                              from GORICCR g
-                             where g.goriccr_sqpr_code = :sqpr_code
-                               and g.goriccr_icsn_code = :icsn_code
-                           """
-            gorriccrValue = sqlObj?.firstRow(sqlQuery,sqpr_code:sqpr_code,icsn_code:icsn_code).goriccr_value
-
-        } catch (Exception Ex) {
-            return null
-        }
-        finally{
-            sqlObj.close()
-        }
-        gorriccrValue
+        IntegrationConfiguration integrationConfiguration = IntegrationConfiguration.fetchByProcessCodeAndSettingName(sqpr_code, icsn_code)
+        integrationConfiguration?.value
     }
+
     /*
     * This method will set the GORRICCR Rule
     * */
 
-    private def updateGoriccrRule(def sqpr_code, def icsn_code, def value) {
-        Sql sqlObj
-        Boolean retValue=false
-        try {
-            sqlObj = new Sql(sessionFactory.getCurrentSession().connection())
-            def sqlQuery = """update GORICCR
-                                 set goriccr_value = :value
-                               where goriccr_sqpr_code = :sqpr
-                                 and goriccr_icsn_code = :icsn
-                            """
-            sqlObj.executeUpdate(sqlQuery,sqpr:sqpr_code,icsn:icsn_code,value:value)
-            retValue=true;
-        } catch (Exception ex) {
-            return retValue
-        }
-        finally{
-            sqlObj.close()
-        }
-        retValue
+    private void updateGoriccrRule(def sqpr_code, def icsn_code, def value) {
+        IntegrationConfiguration integrationConfiguration = IntegrationConfiguration.fetchByProcessCodeAndSettingName(sqpr_code, icsn_code)
+        integrationConfiguration.value = value
+        integrationConfiguration.save(flush: true, failOnError: true)
     }
-
-
-
 }

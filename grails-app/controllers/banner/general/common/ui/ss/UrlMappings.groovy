6ae8c78d47c62488ad238c4dcc61336d679a38cc
Ankur Jain <ankur.jain@ellucian.com>
2019-01-03 15:51:55 +0530

    Reverting to last commit as this fix will be available in different branch - HRU-8280

--- grails-app/controllers/banner/general/common/ui/ss/UrlMappings.groovy
+++ grails-app/controllers/banner/general/common/ui/ss/UrlMappings.groovy
@@ -3,6 +3,11 @@ package banner.general.common.ui.ss
 class UrlMappings {
 
     static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
 
         "/"(view:"/index")
         "500"(view:'/error')

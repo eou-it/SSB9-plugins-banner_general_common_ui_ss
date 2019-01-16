e9852c0bde3d8b4ccecd9c1a72a74f947fd4cd9d
Eshaan_kumar <eshaan.kumar@ellucian.com>
2019-01-16 10:20:27 +0530

    Refactoring and removing unused code in Application.groovy

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -2,31 +2,6 @@
  Copyright 2014-2018 Ellucian Company L.P. and its affiliates.
  ****************************************************************************** */
 
-// ******************************************************************************
-//
-//                       +++ EXTERNALIZED CONFIGURATION +++
-//
-// ******************************************************************************
-//
-// Config locations should be added to the map used below. They will be loaded based upon this search order:
-// 1. Load the configuration file if its location was specified on the command line using -DmyEnvName=myConfigLocation
-// 2. Load the configuration file if it exists within the user's .grails directory (i.e., convenient for developers)
-// 3. Load the configuration file if its location was specified as a system environment variable
-//
-// Map [ environment variable or -D command line argument name : file path ]
-grails.config.locations = [] // leave this initialized to an empty list, and add your locations
-// in the APPLICATION CONFIGURATION section below.
-
-/*
-def locationAdder = ConfigFinder.&addLocation.curry(grails.config.locations)
-
-[BANNER_APP_CONFIG:        "banner_configuration.groovy",
-        customRepresentationConfig: "grails-app/conf/CustomRepresentationConfig.groovy",
-].each { envName, defaultFileName -> locationAdder(envName, defaultFileName) }
-*/
-
-
-
 grails.project.groupId = "net.hedtech" // used when deploying to a maven repo
 grails.databinding.useSpringBinder=true
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format

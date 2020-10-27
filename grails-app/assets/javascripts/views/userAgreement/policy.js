aa5b9d1601ddbf57cfc42e0c80ae6a7e4e625b07
chirayuh <Chirayu.H@ellucian.com>
2020-10-27 18:54:18 +0530

    HRU-9737: Optimizing the code.

--- grails-app/assets/javascripts/views/userAgreement/policy.js
+++ grails-app/assets/javascripts/views/userAgreement/policy.js
@@ -11,15 +11,19 @@ $(document).ready(function () {
         var href = $(this).attr("data-endpoint");
         window.location = href;
     });
+    $('div.termstextdiv').find('blockquote')[0].id="termsText";
     document.addEventListener("keydown", function (e) {
         if(e.key == 'c' && e.altKey) {
             $("#policy-continue").focus();
+            e.preventDefault();
         }
         else if(e.key == 'x'  && e.altKey) {
             $("#policy-exit").focus();
+            e.preventDefault();
         }
         else if(e.key == 'i' && e.altKey) {
-            $('#terms-text').focus();
+            $('.termstextdiv').focus();
+            e.preventDefault();
         }
 
     });
